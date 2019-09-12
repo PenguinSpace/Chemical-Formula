6ea38d57ee272f1880b00d5a25b262439658b268
Nelson Yeung <nelsonyeung97@gmail.com>
2019-09-11 22:13:59 -0400

    adds in isotope labels onto the new perodic table

--- Perodictable.py
+++ Perodictable.py
@@ -27,23 +27,47 @@ for element in elements:
             max_abundt = current_abund
             mass_index = i
     
-    # makes the dictionary 
+    # makes the dictionary with keys being Symbol and values being Mass
     element_mass = mass_elements.iloc[mass_index]['Mass']
-    ptable[mass_elements.iloc[mass_index]['Symbol']] = element_mass
+    ptable[mass_elements.iloc[mass_index]['Symbol']] = round(element_mass, 4)
 
 # makes list of elements
 element_series = list(mass_elements['Name'].unique())
-data = {'Name': element_series, 'Symbol': list(ptable.keys()), 'Monoisotopic Mass': list(ptable.values())}
+data = {'Name': element_series, 'Symbol': list(ptable.keys()), 'Monoisotopic_Mass': list(ptable.values())}
 # print(data)
 
-# puts all the relevant information into a new DataFrame
+# puts all the relevant information into a new DataFrame and saves to a csv file
 PerodicTable = pd.DataFrame(data)
+# print(PerodicTable)
+PerodicTable.to_csv('New_Ptable.csv')
+
+
+# %% codecell
+# I want to reformat the symbols column to use brackets instead of ()
+Isotope = PerodicTable['Symbol'].str.extract(r'(\w{2})', expand=False)
+Isotope_number = PerodicTable['Symbol'].str.extract(r'(\d+)', expand=False)
+Isotope_number = '[' + Isotope_number + ']'
+
+Isotope = Isotope_number + Isotope
+#print(Isotope)
+
+PerodicTable['Isotopes'] = Isotope
 print(PerodicTable)
-        
+
+
+
 
 
 
 # %% codecell
+
+
+
+
+
+
+
+
 
 
 
